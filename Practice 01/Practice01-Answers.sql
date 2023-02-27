edbbd16150c3a1bb2232d81c46dc015298f7f564
Arezoo Dahesh <arezoo.dahesh@gmail.com>
2023-02-27 19:52:53 +0330

    Update answers

--- Practice 01/Practice01-Answers.sql
+++ Practice 01/Practice01-Answers.sql
@@ -16,23 +16,7 @@ HAVING SUM(qty*unitprice) > 10000
 ORDER BY totalvalue DESC;
 GO
 
-
 ------------------------------Q3------------------------------
-/*جواب تمرین سوم در فایل ضمیمه، پیوست شده است*/
--- Query 1
-SELECT empid, COUNT(*) AS numorders
-FROM Sales.Orders
-WHERE orderdate < '20160501'
-GROUP BY empid;
-
-
--- Query 2
-SELECT empid, COUNT(*) AS numorders
-FROM Sales.Orders
-GROUP BY empid
-HAVING MAX(orderdate) < '20160501';
-
-------------------------------Q4------------------------------
 SELECT TOP(3) shipcountry, AVG(freight) AS 'avgfreight'
 FROM Sales.Orders
 WHERE YEAR(orderdate) = '2015'
